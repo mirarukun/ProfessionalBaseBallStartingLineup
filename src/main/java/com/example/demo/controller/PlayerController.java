dbab5f79a1e21a609f60f0433efcade4920c0bec
三浦透 <miraru.395091@gmail.com>
2022-10-16 00:02:24 +0900

    選手登録画面への遷移完了

---
+++ src/main/java/com/example/demo/controller/PlayerController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.service.PlayerService;
+
+@Controller
+public class PlayerController {
+	@Autowired
+	PlayerService playerService;
+	
+	//選手登録画面表示処理
+		@GetMapping("/playerRegister")
+		public ModelAndView playerRegister() {
+			ModelAndView mav = new ModelAndView();
+			mav.setViewName("playerRegister");
+			return mav;
+		}
+}
