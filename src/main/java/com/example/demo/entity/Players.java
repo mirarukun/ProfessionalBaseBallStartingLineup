2f6feff07784132ecdaf4d0d09c2260683705009
三浦透 <miraru.395091@gmail.com>
2022-10-15 22:32:11 +0900

    選手作成のためのエンティティ準備

---
+++ src/main/java/com/example/demo/entity/Players.java
@@ -0,0 +1,35 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class Players {
+	
+	@Id
+	@Column //カラム名の指定
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //自動採番
+	private int id;
+
+	@Column
+	private String name;
+
+	@Column
+	private String strongArm;
+
+	@Column
+	private String mainPosition;
+	
+	@Column
+	private String subPosition;
+}
